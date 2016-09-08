90a27a2094f81e2d4c746d22be8cd4ddc4f6db3d
DawidMyslak <dawid.myslak@teamwork.com>
2016-09-08 10:22:27 +0100

    Sample list of components

--- src/App.js
+++ src/App.js
@@ -2,17 +2,74 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const TestWidget1 = (props) => {
+  return (
+    <tr>
+      <td>{props.widget.id}</td>
+      <td>{props.widget.type}</td>
+      <td>{props.widget.title}</td>
+    </tr>
+  );
+};
+
+const TestWidget2 = (props) => {
+  return (
+    <tr>
+      <td>{props.widget.id}</td>
+      <td>{props.widget.type}</td>
+      <td>{props.widget.title}</td>
+    </tr>
+  );
+};
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      timeline: [
+        {
+          id: 1,
+          title: 'Title1',
+          type: 'TestWidget1',
+          data: {
+          }
+        },
+        {
+          id: 2,
+          title: 'Title2',
+          type: 'TestWidget2',
+          data: {
+          }
+        }
+      ]
+    };
+  }
+  
   render() {
+    let timeline = this.state.timeline.map(widget => {
+      let result;
+      if (widget.type === 'TestWidget1') {
+        result = <TestWidget1 key={widget.id} widget={widget} />
+      }
+      else if (widget.type === 'TestWidget2') {
+        result = <TestWidget2 key={widget.id} widget={widget} />
+      }
+      return result;
+    });
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React!</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <table>
+          <tbody>
+            {timeline}
+          </tbody>
+        </table>
       </div>
     );
   }
